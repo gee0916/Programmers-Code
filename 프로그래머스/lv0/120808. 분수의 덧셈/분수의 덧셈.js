7003913d5fef4fbbca09b2a5be434cf0da4b33f8
gee0916 <114209738+gee0916@users.noreply.github.com>
2023-08-06 19:38:05 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -4,7 +4,7 @@ function solution(numer1, denom1, numer2, denom2) {
     var numer = (numer1*denom2) + (numer2*denom1)
     var denom = denom1*denom2
     
-    let min=0; //최소공약수
+    let min=0; //최대공약수
     for(let i=1; numer<denom ? i<=numer : i<=denom; i++) {
         if(numer%i==0 && denom%i==0) {
             min = i;
