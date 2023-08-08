d3de18cf051169b0fc2ea784d6f366ba3895f44d
gee0916 <114209738+gee0916@users.noreply.github.com>
2023-08-08 19:36:48 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.20 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,21 @@
+function solution(array) {
+  let cnt = {};
+  let maxCnt = 0;
+  let result = 0;
+
+  array.forEach((x) => {
+      if(!cnt[x]) {
+          cnt[x]=1;
+      }else {
+          cnt[x]+=1
+      }
+  });
+
+   // cnt[x] = (cnt[x] || 0) + 1;
+    
+  maxCnt = Math.max(...Object.values(cnt));
+  result = Object.keys(cnt).filter((x) => cnt[x] === maxCnt);
+    result = result.length === 1 ? Number(result) : -1
+
+  return result;
+}
