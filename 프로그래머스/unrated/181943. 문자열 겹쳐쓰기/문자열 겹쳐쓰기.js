b4b34bf700471a9886f791e28d89fd6b10b4417b
gee0916 <114209738+gee0916@users.noreply.github.com>
2023-08-09 18:04:56 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,5 +1,8 @@
 function solution(my_string, overwrite_string, s) {
     
+//     다른풀이
+    // my_string.substring(0, s) + overwrite_string + my_string.substring(s+overwrite_string.length)
+    
     const ms = [...my_string]
     ms.splice(s, overwrite_string.length, overwrite_string)
     return ms.join('');
