abc6b2785187e78f1826efaf5505af3541bc3b96
gee0916 <114209738+gee0916@users.noreply.github.com>
2023-08-21 17:14:43 +0900

    [level 0] Title: 팩토리얼, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -0,0 +1,21 @@
+function factorial(num) {
+    if(num<=1) {
+        return num
+    }
+    return num * factorial(num-1)
+}
+
+function solution(n) {
+
+    let result = 0;
+    
+    for(;;) {
+        result++;
+       if(factorial(result) > n) {
+           return --result
+       }
+        factorial(result)
+    }
+    
+    return result;
+}
