ea41bf0f651559a375244a779d6c8434b761a1fc
gee0916 <114209738+gee0916@users.noreply.github.com>
2023-08-29 23:54:00 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,15 +1,6 @@
 function solution(my_string) {
-    
-    let arr = my_string.split(' ')
-    let result = Number(arr[0]);
-    
-    for (let i=0; i<arr.length; i++) {
-        if(arr[i] === "+") {
-            result+=Number(arr[i+1])
-        }else if(arr[i] === "-") {
-            result-=Number(arr[i+1])
-        }
-    }
-    
-    return result;
+  let arr = my_string.split(" ");
+  return arr[1] === "+"
+    ? Number(arr[0]) + Number(arr[2])
+    : Number(arr[0]) - Number(arr[2]);
 }
