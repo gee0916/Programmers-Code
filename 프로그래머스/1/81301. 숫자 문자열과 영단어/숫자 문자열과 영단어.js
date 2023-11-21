6687546e7698c981672e9014c659478fa63c6956
gee0916 <114209738+gee0916@users.noreply.github.com>
2023-11-21 20:26:19 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,22 @@
+function solution(s) {
+
+    let change = {
+    "zero":0,
+    "one":1,
+    "two":2,
+    "three":3,
+    "four":4,
+    "five":5,
+    "six":6,
+    "seven":7,
+    "eight":8,
+    "nine":9
+    }
+    
+    for(let key of Object.keys(change)){
+        s=s.replaceAll(key, change[key])
+    }
+    
+    
+    return Number(s);
+}
