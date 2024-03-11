36fa633829652fd7d60e4cf71d96c84c8740a396
gee0916 <114209738+gee0916@users.noreply.github.com>
2024-03-11 22:34:41 +0900

    [level 1] Title: 폰켓몬, Time: 2.11 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -3,6 +3,6 @@ function solution(nums) {
     let len = nums.length/2
     let set = new Set(nums.sort())
     
-    return len < [...set].length ? len : [...set].length;
+    return len < set.size ? len : set.size;
     
 }
