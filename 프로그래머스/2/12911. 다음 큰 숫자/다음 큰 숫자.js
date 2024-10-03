278d132c52eb049ba5245c86fef3efe81323cf57
gee0916 <114209738+gee0916@users.noreply.github.com>
2024-10-03 21:13:21 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    
+    let nCnt = 0;
+    let rCnt = 0;
+    let result = 0;
+    
+   nCnt = n.toString(2).split('0').join('').length;
+    
+    while (true) {
+        if(nCnt===rCnt) {
+            return n;
+        }
+        n++;
+        rCnt = n.toString(2).split('0').join('').length;
+    }
+    
+}
